560875cd791dbde221d9beb6f3d2348b58d72704
reemsalsm <114924100+reemsalsm@users.noreply.github.com>
2025-01-27 19:31:14 +0200

    Update plank.py

--- plank.py
+++ plank.py
@@ -134,3 +134,10 @@ def plank():
 
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    # Uncomment to generate reference angles
+    # ref_squat('squat.mp4')
+
+    # Uncomment to start squat tracking
+    plank()
